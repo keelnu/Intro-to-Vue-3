9dc1b27ceec93393f8d4c8767931472a49c668ab
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2021-04-28 18:34:22 -0400

    working on challenge

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      image: './assets/images/socks_blue.jpg',
+      inStock: false,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+      ],
+      // imageStyles: {
+      //   background-color: variants.color,
+      //   out-of-stock-img: !inStock,
+      // },
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+});
