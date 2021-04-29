e98e161468dc40ce25654ca5abd732e91259bda7
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2021-04-29 09:56:55 -0400

    L8 module complete

--- main.js
+++ main.js
@@ -5,19 +5,19 @@ const app = Vue.createApp({
       product: 'Socks',
       brand: 'Booboo',
       onSale: true,
+      selectedVariant: 0,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
         { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
       ],
-      selectedVariant: 0,
     };
   },
   methods: {
     addToCart() {
       this.cart += 1;
     },
-    updateImage(index) {
+    updateVariant(index) {
       this.selectedVariant = index;
     },
   },
@@ -27,7 +27,7 @@ const app = Vue.createApp({
     },
     sale() {
       if (this.onSale) {
-        return `${this.brand} ${this.product} is on sale.`;
+        return this.brand + ' ' + this.product + ' is on sale.';
       }
       return '';
     },
