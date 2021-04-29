7e73f763c3fb4dbffce7aeff89b189751377a060
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2021-04-29 09:46:50 -0400

    L8 module in progress

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      brand: 'Booboo',
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+      ],
+      selectedVariant: 0,
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+    updateImage(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    brandSale() {
+      return this.brand + ' ' + this.product + ' is on sale.'
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+  },
+});
